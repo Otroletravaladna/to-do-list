79ebd1a87f0c8ed0e21919ae34338a390f47efbc
Otroletravaladna <crnana98@gmail.com>
2023-02-08 17:34:23 -0300

    Add todo form and json object storage

--- src/data.js
+++ src/data.js
@@ -1,3 +1,10 @@
-export function test() {
-    console.log("it's aliveee");
+export const Todo = function() {
+    this.title = document.querySelector("#title").value;
+    this.desc = document.querySelector("#desc").value;
+    this.dueDate = document.querySelector("#date").value;
+    this.priority = document.querySelector("#priority").value;
+    this.project = document.querySelector("#project").value;
+    this.check = false;
 }
+
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
 import "./style.css";
-import { test } from "./data.js";
+import { Todo } from "./data.js";
 
-test();
+const form = document.querySelector("form");
+const popUpNote = (() => {
+    document.querySelector(".open-button").addEventListener("click", () => {
+        document.getElementById("myForm").style.display = "block";
+    });
+      
+    document.querySelector(".btn.cancel").addEventListener("click", () => {
+        document.getElementById("myForm").style.display = "none";
+    });
+
+})();
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let newTodo = new Todo();
+    archive.push(newTodo);
+    
+    localStorage.setItem("todoList", JSON.stringify(archive));
+});
+
+let archive = JSON.parse(localStorage.getItem("todoList")) || [];
+
+console.table(archive);
